db07de61eb4c2d9cd8750dbd073a33a6b91bab99
Anchal Singh <anchal.singh6890@gmail.com>
2025-08-01 18:54:30 +0530

    task4_exception_handling.py

--- Core Tasks/task4_exception_handling.py
+++ Core Tasks/task4_exception_handling.py
@@ -1,9 +1,21 @@
 try:
-    num1 = int(input("Enter number: "))
-    num2 = int(input("Enter another number: "))
-    result = num1 / num2
-    print("Result:", result)
+
+ num1 = int(input("Enter first number: "))
+
+ num2 = int(input("Enter second number: "))
+
+ result = num1 / num2
+
+ print("Result:", result)
+
 except ZeroDivisionError:
-    print("Error: Cannot divide by zero")
+
+ print("Error: Cannot divide by zero")
+
 except ValueError:
-    print("Error: Invalid input")
+
+ print("Error: Please enter only numbers")
+
+finally:
+
+ print("Calculation completed.")
